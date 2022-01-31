dffd60574f1a90e21913d39c8916ecf3a0fa2728
matthaussouza <71097104+matthaussouza@users.noreply.github.com>
2022-01-31 20:51:24 -0300

    Ajustando URLs

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './utils';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://expin-back.herokuapp.com/',
 });
 
 api.interceptors.request.use(function (config) {
---
+++ src/consts.js
@@ -0,0 +1,4 @@
+export default{
+    API_URL: 'https://expin-back.herokuapp.com/',
+    OAPI_URL: 'https://expin-back.herokuapp.com/',
+}
